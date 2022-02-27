c2e71669dac4ff8033079b62dee5d72e84f50a6e
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2022-02-27 21:27:55 +0530

    Update strings.py

--- Yukki/Plugins/custom/strings.py
+++ Yukki/Plugins/custom/strings.py
@@ -20,7 +20,7 @@ START_BUTTON_GROUP = InlineKeyboardMarkup(
     [   
         [
             InlineKeyboardButton(
-                text="💠 Commands and Menu", callback_data="command_menu"
+                text="💠 Menu", callback_data="command_menu"
             ),
             InlineKeyboardButton(
                 text="🔧 Settings", callback_data="settingm"
@@ -45,7 +45,7 @@ START_BUTTON_PRIVATE = InlineKeyboardMarkup(
         ],
         [   
             InlineKeyboardButton(
-                text="💠 Commands and Menu", callback_data="command_menu"
+                text="💠 Menu", callback_data="command_menu"
             ),                       
         ],
         [
