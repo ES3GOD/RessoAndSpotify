93d83fbc141967bdebc8c6fdc1c900fc397783d1
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2022-03-01 15:25:13 +0530

    Update thumbnails.py

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -73,8 +73,8 @@ async def gen_thumb(thumbnail, title, userid, status, views, duration, channel):
 
     # title
     title1 = truncate(title)
-    image4.text((670, 270), text=title1[0], fill="white", font = font3, align ="left") 
-    image4.text((670, 300), text=title1[1], fill="white", font = font3, align ="left") 
+    image4.text((670, 250), text=title1[0], fill="white", font = font3, align ="left") 
+    image4.text((670, 280), text=title1[1], fill="white", font = font3, align ="left") 
 
     # description
     views = f"Views : {views}"
