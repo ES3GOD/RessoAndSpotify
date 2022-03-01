137c2b35b1e4e45ac4bcd56444a8141843739735
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2022-03-01 14:41:27 +0530

    Ye

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -81,9 +81,9 @@ async def gen_thumb(thumbnail, title, userid, status, views, duration, channel):
     duration = f"Duration : {duration} Mins"
     channel = f"Channel : {channel}"
 
-    image4.text((670, 350), text=views, fill="white", font = font4, align ="left") 
-    image4.text((670, 400), text=duration, fill="white", font = font4, align ="left") 
-    image4.text((670, 450), text=channel, fill="white", font = font4, align ="left")
+    image4.text((690, 350), text=views, fill="white", font = font4, align ="left") 
+    image4.text((690, 400), text=duration, fill="white", font = font4, align ="left") 
+    image4.text((690, 450), text=channel, fill="white", font = font4, align ="left")
 
     image2.save(f"cache/final{userid}.png")
     os.remove(f"cache/thumb{userid}.jpg")
